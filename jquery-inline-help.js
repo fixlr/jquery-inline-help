0e85cb96faa71b7bb9270f8cb2dab431c810246c
Nathan Fixler <nathan@fixler.org>
2011-09-27 12:36:06 -0400

    Updated demo to include multiple inputs

--- jquery-inline-help.js
+++ jquery-inline-help.js
@@ -1,17 +1,19 @@
-(function( $ ){
+(function($){
   $.fn.inlineHelp = function() {
     this.each(function() {
-      input = $(this)
-      help_text = $('<span class="help">'+input.attr('data-help')+'</span>').hide()
-      input.after(help_text)
+      input = $(this);
+      help_text = $('<span class="help">'+input.attr('data-help')+'</span>').hide();
+      input.after(help_text);
 
-      input.click(function() {
-        help_text.show()
+      (function(input, help_text){
+        input.click(function() {
+          help_text.show();
 
-        $(this).blur(function() {
-          help_text.hide()
-        })
-      })
-    })
+          input.blur(function() {
+            help_text.hide();
+          });
+        });
+      })(input, help_text);
+    });
   };
-})( jQuery );
+})(jQuery);
